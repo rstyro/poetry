e40ab3cb85d0817031c2bed65f06741d89333aec
rstyro <1006059906@qq.com>
2022-08-17 10:32:05 +0800

    添加公共的实体类

---
+++ src/main/java/top/rstyro/poetry/commons/ApiExceptionCode.java
@@ -0,0 +1,28 @@
+package top.rstyro.poetry.commons;
+
+public enum ApiExceptionCode {
+    SUCCESS(200,"成功"),
+    FAILED(400,"请求失败"),
+    ERROR(500,"不知名错误"),
+    ERROR_NULL(501,"空指针异常"),
+    ERROR_CLASS_CAST(502,"类型转换异常"),
+    ERROR_RUNTIME(503,"运行时异常"),
+    ERROR_IO(504,"上传文件异常"),
+    ERROR_METHOD(505,"请求方法错误"),
+    ;
+    private String message;
+    private int status;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    ApiExceptionCode( int status,String message) {
+        this.message = message;
+        this.status = status;
+    }
+}
